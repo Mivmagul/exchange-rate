10f84e159d8c7e3536ed56ea61cf1cdbe53b6ba9
vadym kolomiiets <vadym.kolomiiets@nordstrom.com>
2025-03-27 21:13:15 +0200

    Enabling caching

--- src/main/java/com/mivmagul/exchangerate/ExchangeRateApplication.java
+++ src/main/java/com/mivmagul/exchangerate/ExchangeRateApplication.java
@@ -2,7 +2,9 @@ package com.mivmagul.exchangerate;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
+@EnableCaching
 @SpringBootApplication
 public class ExchangeRateApplication {
     public static void main(String[] args) {
